9fb5c47239231c1112af4e137599ac621db5d5bd
AntonioVT <tony-vt@hotmail.com>
2016-12-02 09:57:01 -0600

    Animations, models, assets

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+    private NavMeshAgent agent;
+    public Transform tModel;
+    public Transform player;
+    public float yOffset;
+    private Animator anim;
+
+	void Start () {
+        SetReferences();
+        StartCoroutine(DelayAnimator());
+	}
+
+	void Update () {
+        if (anim.enabled)
+        {
+            Follow();
+            Rotate();
+        }
+    }
+
+    void SetReferences()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        if (anim == null)
+        {
+            anim = GetComponentInChildren<Animator>();
+        }
+    }
+
+    void Follow()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+
+    void Rotate()
+    {
+        tModel.LookAt(player);
+        Quaternion qt = tModel.rotation;
+        Vector3 euler = qt.eulerAngles;
+        euler.y += yOffset;
+        euler.x = 0;
+        tModel.rotation = Quaternion.Euler(euler);
+    }
+
+    IEnumerator DelayAnimator()
+    {
+        anim.enabled = false;
+        yield return new WaitForSeconds(Random.Range(0, 2.0f));
+        anim.enabled = true;
+    }
+}
---
+++ Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenDoor : MonoBehaviour
+{
+
+    public AnimationClip[] clips;// clipOpen, clipClose;
+    [SerializeField]
+    private Animation anim;
+    private bool bOpen = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+            ToggleDoor();
+    }
+
+    void ToggleDoor()
+    {
+        if (!anim.isPlaying)
+        {
+            anim.clip = clips[bOpen ? 1 : 0];
+            anim.Play();
+            bOpen = !bOpen;
+        }
+    }
+}
